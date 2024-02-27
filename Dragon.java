a05b01b57311fe095853f73803a1c1463ae67685
faujiiiahhh <framandaadelia@gmail.com>
2024-02-27 13:49:47 +0700

    Dragon10.java

---
+++ Dragon.java
@@ -0,0 +1,42 @@
+package JOBSHEET2;
+
+public class Dragon{
+
+    int x, y, width, height;
+
+    void moveLeft() {
+        x -= 1;
+        if (x < 0 || x > width) {
+            detectCollision(x, y);
+        }
+    }
+
+    void moveRight() {
+        x += 1;
+        if (x < 0 || x > width) {
+            detectCollision(x, y);
+        }
+    }
+
+    void moveUp() {
+        y -= 1;
+        if (y < 0 || y > height) {
+            detectCollision(x, y);
+        }
+    }
+
+    void moveDown() {
+        y += 1;
+        if (y < 0 || y > height) {
+            detectCollision(x, y);
+        }
+    }
+
+    void printPosition() {
+        System.out.printf("Anda Berada di X : %d Y : %d\n", x  , y );
+    }
+
+    void detectCollision(int x, int y) {
+        System.out.println("Game Over");
+    }
+}
